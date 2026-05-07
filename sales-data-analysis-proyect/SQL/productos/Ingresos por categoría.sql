338b5e1d2e68316fe4b762e651d54a0ed6fc6bcb
kiara <108772473+kiahal@users.noreply.github.com>
2026-05-07 10:42:54 -0300

    Delete sales-data-analysis-proyect/SQL directory

--- sales-data-analysis-proyect/SQL/productos/Ingresos por categoría.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    c.Nombre_Categoria,
-    FORMAT(SUM(dv.Cantidad * dv.Precio_Unidad), 'C0', 'es-AR') AS ingresos
-FROM Detalle_Venta dv
-JOIN Productos p ON dv.Id_Prod = p.Id_Prod
-JOIN Categoria c ON p.Id_Categoria = c.Id_Categoria
-GROUP BY c.Nombre_Categoria
-ORDER BY SUM(dv.Cantidad * dv.Precio_Unidad) DESC;
--- sales-data-analysis-proyect/SQL/productos/Productos más vendidos.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    p.Nombre_Prod,
-    FORMAT(SUM(dv.Cantidad), 'N0', 'es-AR') AS cantidad_vendida
-FROM Detalle_Venta dv
-JOIN Productos p ON dv.Id_Prod = p.Id_Prod
-GROUP BY p.Nombre_Prod
-ORDER BY SUM(dv.Cantidad) DESC;
